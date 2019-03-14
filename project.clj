5c263d119893c57762ef0f9f47deb10e9a552a3f
Roman Klauser <roman.klauser.email@gmail.com>
2019-03-14 19:29:50 +0100

    running http server

--- project.clj
+++ project.clj
@@ -6,6 +6,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
-                 [clj-time "0.14.0"]
-                 [compojure "1.6.0"]]
+                 [compojure "1.6.0"]
+                 [com.novemberain/monger "3.1.0"]
+                 [clojure-csv "2.0.2"]
+                 [org.clojure/data.json "0.2.6"]
+                 [jumblerg/ring-cors "2.0.0"]]
   :main clojure-simple-http.core)
--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -2,33 +2,58 @@
   (:require [org.httpkit.server :refer [run-server]]
             [clj-time.core :as t]
             [compojure.core :refer :all]
-            [compojure.route :as route]))
-(import java.util.Date)
+            [compojure.route :as route]
+            [monger.core :as mg]
+            [monger.collection :as mc]
+            [clojure-csv.core :as csv]
+            [clojure.data.json :as json]
+            [jumblerg.middleware.cors :refer [wrap-cors]]
+            )
+  (:import [com.mongodb MongoOptions ServerAddress])
+  (:gen-class))
 
-; returns current time.
-(defn get-time
+
+(defn csv-to-sets
+  [file-name]
+  (let [
+        imports (csv/parse-csv (slurp file-name) :delimiter \;)
+        transfer-keys (first imports)
+        transfer-data (drop 1 imports)]
+    (map (fn [transfer-data-set]
+           (reduce conj
+                   (map #(hash-map (keyword (first %)) (second %))
+                        (map vector transfer-keys transfer-data-set))))
+         transfer-data)))
+
+(defn get-transfers
   []
-  (let [response {:status  200
-                  :headers {"Content-Type" "text/html"}
-                  :body    (str (t/time-now))}]
-    response))
+  (let [^MongoOptions opts (mg/mongo-options {:threads-allowed-to-block-for-connection-multiplier 300})
+        ^ServerAddress sa (mg/server-address "127.0.0.1" 27017)
+        conn (mg/connect sa opts)
+        db (mg/get-db conn "mydb")]
+    (mc/find-maps db "documents")))
 
-; returns current date.
-(defn get-date
-  [greet-name]
-  (let [response {:status  200
-                  :headers {"Content-Type" "text/html"}
-                  :body    (str (.getTime (java.util.Date.)))}]
-    response))
+(defn add-record
+  [record]
+  (let [^MongoOptions opts (mg/mongo-options {:threads-allowed-to-block-for-connection-multiplier 300})
+        ^ServerAddress sa (mg/server-address "127.0.0.1" 27017)
+        conn (mg/connect sa opts)
+        db (mg/get-db conn "mydb")]
+    (mc/insert db "documents" record)
+    "done"))
 
 ; define routes.
-(defroutes app
+(defroutes my-routes
            (GET "/" [] "<h1>Welcome</h1>")
-           (GET "/get-time" [] (get-time))
-           (GET "/get-date" [] (get-date))
-           (route/not-found "<h1>Page not found</h1>"))
+           (GET "/foo" [] "foo")
+           (GET "/data" [] (json/write-str (map (fn [t] (update t :_id (fn [id] (.toString id)))) (get-transfers))))
+           (GET "/test" [] (json/write-str (csv-to-sets "export.csv")))
+           (POST "/add" req (add-record req))
+           (route/not-found "<h1>Page - NOT - found</h1>"))
+
 
 ; starting point of server.
 (defn -main [& args]
-  (run-server app {:port 8080})
+  (run-server (wrap-cors my-routes #".*") {:port 8080})
   (println "Server started on port 8080"))
+
